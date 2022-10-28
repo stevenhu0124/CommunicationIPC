5d576b16b4658a65ca7ad58d94860f38eaa11c11
steven-hu <steven.hu@hp.com>
2022-10-28 09:31:44 +0800

    Init project

---
+++ CommunicationIPC/HyperXCenter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CommunicationIPC
+{
+    /// <summary>
+    /// Simulation of multiple APs using the same DLL
+    /// </summary>
+    public class HyperXCenter
+    {
+    }
+}
